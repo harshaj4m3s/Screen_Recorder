bd26cce89b226810950c4442ccbd5cf9363e576f
Harshavardhan <harshavardhan@protonmail.ch>
2020-08-26 13:34:37 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Screen Recorder using python
+A python script for recording your screen
+
+This script provides a command-line interface for recording the screen. This is still in development process.
+(The script supports only Python 3)
+## Prerequisites
+This should work after the installing the following packages:
+    * pyautogui
+    * opencv-python
+    * numpy
+    * keyboard
+You can just install the required packages with the following command
+```
+pip install -r requirements.txt
+```
+or
+```
+python3 -m pip install -r requirements.txt
+```
+
+## To-Do
+Here are the features that I would like to include in the future.
+* add web camera support to screen recording
+* add audio support, i.e., both microphone and device audio (currently I am working on this).
+* add GUI support (maybe)
+
+Please don't expect the to-do features in-time because this is kind of fun-time project for me.
+
+If you have any suggestions or want to add any features to this, or modify something in this, feel free to contact me at harshajames@outlook.com
---
+++ main.py
@@ -0,0 +1,55 @@
+import pyautogui
+import cv2
+import numpy as np
+import keyboard
+import threading
+import argparse
+import os
+from datetime import datetime
+
+
+def exit_handler():
+    global STOP
+    STOP = True
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Records screen and store it in mp4 format with 20 fps by default. \
+            Recordings can be found at recordings folder in relative path. \
+                Press ctrl+alt+s to STOP recording ')
+    parser.add_argument('--output', dest='output',
+                        required=False, help="target file name   <<OPTIONAL>>")
+    parser.add_argument('--audio', dest='audio', action='store_true',
+                        help='Boolean flag for including Audio in the Recording')
+    parser.add_argument('--cam', dest='cam', action='store_true',
+                        help='Boolean flag for including Camera feed in the Recording')
+    return parser.parse_args()
+
+
+def main():
+    ''''''
+    args = parse_arguments()
+    _file_name = args.output
+    _include_audio = args.audio
+    if not _file_name:
+        _file_name = 'recording_'+str(int(datetime.timestamp(datetime.now())))
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    keyboard.add_hotkey('ctrl+alt+s', exit_handler)
+    out = cv2.VideoWriter(os.path.join(
+        'recordings', _file_name+'.mp4'), fourcc, 20.0, SCREEN)
+    while not STOP:
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+    print('successfully saved to  :  {}.mp4'.format(_file_name))
+    cv2.destroyAllWindows()
+    out.release()
+
+
+SCREEN = pyautogui.size()
+STOP = False
+
+if __name__ == '__main__':
+    main()
